bb788d023e14d40a54c794fe0a0580b4e6ee25ca
AIL <andrealincoln42@gmail.com>
2024-03-13 23:29:05 -0400

    made front page less ugly

--- my-pregnancy-tracker/src/HomePage.js
+++ my-pregnancy-tracker/src/HomePage.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { ReactComponent as StorkIcon } from './assets/StorkBlue.svg'; // Adjust the path as necessary
 
 function HomePage() {
   const [dueDate, setDueDate] = useState('');
@@ -7,26 +12,35 @@ function HomePage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Redirect to the WeekByWeekPage with the dueDate in the URL
     navigate(`/week/${dueDate}`);
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to My Pregnancy Tracker!</h1>
-      <p>This tool helps you track your pregnancy week by week.</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your due date:
-          <input
-            type="date"
-            value={dueDate}
-            onChange={(e) => setDueDate(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Start tracking</button>
-      </form>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#eafcf5' }}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent style={{ textAlign: 'center' }}>
+          <h1>Welcome to <span style={{ fontWeight: 'bold' }}>My Pregnancy Tracker</span>!</h1>
+          <p>This tool will let you make a link to a page which will track your pregnancy week by week!</p>
+          <p>Share the link with a partner, friends, family, or just yourself!</p>
+          <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
+            <TextField
+              id="date"
+              label="Due Date"
+              type="date"
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+            />
+                <Button variant="contained" type="submit" endIcon={<StorkIcon style={{ width: '30px', height: '30px' }} />} style={{ marginTop: '20px' }}>
+                  Start tracking
+                </Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
