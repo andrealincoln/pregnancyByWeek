295d31f8dc2b9532d3d960b1acd8ec3cb6de0c0d
AIL <andrealincoln42@gmail.com>
2024-03-14 00:42:56 -0400

    fixing some default react app stuff

--- my-pregnancy-tracker/src/App.js
+++ my-pregnancy-tracker/src/App.js
@@ -7,7 +7,7 @@ import Topbar from "./Topbar";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
         <Topbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
